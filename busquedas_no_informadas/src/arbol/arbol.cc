eefc394a02e132db4cfb927d9c0592e0359d5fa1
alu0101571669 <alu0101571669@ull.edu.es>
2024-09-26 18:43:16 +0100

    Recorrido DFS implementado

--- busquedas_no_informadas/src/arbol/arbol.cc
+++ busquedas_no_informadas/src/arbol/arbol.cc
@@ -38,88 +38,71 @@ Arbol::Arbol(ifstream& file) {
 */
 
 void Arbol::DFS(int nodo_origen, int nodo_destino, ofstream& file) {
-  stack<int> pila; // Pila para gestionar los nodos a explorar
-  set<int> visitados; // Conjunto de nodos visitados
-  map<int, Nodo> camino; // Mapa para guardar el camino
+  stack<int> pila; // Pila para almacenar los nodos
+  set<int> visitados; // Conjunto para almacenar los nodos visitados
+  map<int, Nodo> camino; // Mapa para almacenar el camino
   double costo_total = 0.0;
   int iteracion = 1;
 
-  // Información inicial del grafo
+  // Información inicial
   file << "Número de nodos del grafo: " << GetNumNodos() << endl;
   file << "Número de aristas del grafo: " << GetAristas() << endl;
   file << "Vértice origen: " << nodo_origen << endl;
   file << "Vértice destino: " << nodo_destino << endl;
 
-  // Insertamos el nodo inicial en la pila
   pila.push(nodo_origen);
   visitados.insert(nodo_origen);
+  vector<int> generados = {nodo_origen}, inspeccionados;
 
-  vector<int> nodos_generados;
-  vector<int> nodos_inspeccionados;
-
-  // Realizar la búsqueda en profundidad
   while (!pila.empty()) {
     int nodo_actual = pila.top();
     pila.pop();
-    nodos_inspeccionados.push_back(nodo_actual);
-
-    // Si encontramos el nodo destino, reconstruimos el camino y calculamos el costo
+    inspeccionados.push_back(nodo_actual);
     if (nodo_actual == nodo_destino) {
-      stack<int> camino_inverso;
-      int temp = nodo_destino;
-      while (temp != nodo_origen) {
-        camino_inverso.push(temp);
+      stack<int> camino_reverso;
+      for (int temp = nodo_destino; temp != nodo_origen; temp = camino[temp].GetPadre()) {
+        camino_reverso.push(temp);
         costo_total += camino[temp].GetCoste();
-        temp = camino[temp].GetPadre();
       }
-      camino_inverso.push(nodo_origen);
-      // Imprimir iteración final
+      camino_reverso.push(nodo_origen);
+
+      // Última iteración
       file << "Iteración " << iteracion << endl;
       file << "Nodos generados: ";
-      for (int nodo : nodos_generados) file << nodo << " ";
-      file << endl;
-      file << "Nodos inspeccionados: ";
-      for (int nodo : nodos_inspeccionados) file << nodo << " ";
-      file << endl;
-
-      // Escribir el camino en el archivo
-      file << "Camino: ";
-      while (!camino_inverso.empty()) {
-        file << camino_inverso.top();
-        camino_inverso.pop();
-        if (!camino_inverso.empty()) file << " - ";
-      }
-      file << endl;
-      file << "Costo: " << costo_total << endl;
+      for (int n : generados) file << n << " ";
+        file << endl << "Nodos inspeccionados: ";
+        for (int n : inspeccionados) file << n << " ";
+        file << endl << "Camino: ";
+        while (!camino_reverso.empty()) {
+          file << camino_reverso.top();
+          camino_reverso.pop();
+          if (!camino_reverso.empty()) file << " - ";
+        }
+        file << endl << "Costo: " << costo_total << endl;
+        cout << "Se ha encontrado un camino, para visualizarlo consulte el fichero de salida" << endl;
       return;
     }
-    // Obtener todos los hijos del nodo inicial
     auto rango_hijos = arbol_.equal_range(nodo_actual);
-    nodos_generados.clear(); // Limpiar los nodos generados
-    for (auto it = rango_hijos.first; it != rango_hijos.second; it++) {
+    generados.clear();
+    for (auto it = rango_hijos.first; it != rango_hijos.second; ++it) {
       Nodo hijo_nodo = it->second;
       int hijo = hijo_nodo.GetHijo();
-      if (visitados.find(hijo) == visitados.end()) {
-        // No ha sido visitado, lo agregamos a la pila
+      if (!visitados.count(hijo)) {
         pila.push(hijo);
         visitados.insert(hijo);
-        // Guardamos el nodo y su relación con el padre
         camino[hijo] = hijo_nodo;
-        // Agregamos el nodo a la lista de nodos generados
-        nodos_generados.push_back(hijo);
+        generados.push_back(hijo);
       }
     }
-    // Imprimir la iteración actual
+    // Imprimir iteración
     file << "Iteración " << iteracion++ << endl;
     file << "Nodos generados: ";
-    for (int nodo : nodos_generados) file << nodo << " ";
-    file << endl;
-    file << "Nodos inspeccionados: ";
-    for (int nodo : nodos_inspeccionados) file << nodo << " ";
+    for (int n : generados) file << n << " ";
+    file << endl << "Nodos inspeccionados: ";
+    for (int n : inspeccionados) file << n << " ";
     file << endl;
   }
-
-  cout << "No se encontró un camino entre los nodos " << nodo_origen << " y " << nodo_destino << endl;
+  cout << "No se encontró un camino entre " << nodo_origen << " y " << nodo_destino << endl;
 }
 
 /**
--- busquedas_no_informadas/src/main.cc
+++ busquedas_no_informadas/src/main.cc
@@ -1,43 +1,34 @@
 #include "nodo/nodo.h"
 #include "arbol/arbol.h"
+#include "options/options.h"
 
 int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    cout << "Pruebe --help para mas información" << endl;
-    return EXIT_FAILURE;
-  }
-  else if (string(argv[1]) == "--help") {
-    cout << "Modo de uso: " << argv[0] << " " << "<fichero>" << endl;
-    return EXIT_SUCCESS;
-  }
-  ifstream file(argv[1]);
+  auto options = parse_args(argc, argv);
+  
+  ifstream file(options->file_in);
+  ofstream file_out(options->file_out);
+  int nodo_origen = options->nodo_origen;
+  int nodo_destino = options->nodo_destino;
   if (!file.is_open()) { // Comprobamos que el fichero se pudo abrir correctamente
     cout << "No se pudo abrir el fichero" << endl;
     return EXIT_FAILURE;
   }
+  
   // Leemos los datos del fichero;
   Arbol arbol(file);
-  ofstream file_out("salida.txt");
-  cout << arbol;
-  arbol.DFS(1, 15, file_out);
-  
-
-
-
-
-
-
-
-
+  cout << "Opción 1: Algortimo de busqueda en profundidad (DFS)" << endl;
+  cout << "Opción 2: Algortimo de busqueda en anchura (BFS)" << endl;
+  int opcion;
+  cout << "Introducir opción: ";
+  cin >> opcion;
+  if (opcion == 1) arbol.DFS(nodo_origen, nodo_destino, file_out);
+  else if (opcion == 2) cout << "Opción no implementada" << endl;
+  else {
+    cout << "Opción no válida" << endl;
+    return EXIT_FAILURE;
+  }
+  // Cerramos ficheros
+  file.close();
+  file_out.close();
   return 0;
-
-
-
-
-
-
-
-
-
-
 }
