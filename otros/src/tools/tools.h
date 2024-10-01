5ecf6935320e9d9961a5a7cf87c45585880e39b9
Franco Alla <114768371+frannelfran@users.noreply.github.com>
2024-10-01 19:50:16 +0100

    Delete otros directory

--- otros/src/tools/tools.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-#include <fstream>
-
-using namespace std;
-
-vector<vector<double>> ConstruirMatrizCoste(ifstream&);
-void MostrarMatriz(const vector<vector<double>>&);
