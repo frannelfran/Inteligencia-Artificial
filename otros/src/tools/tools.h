fddd3edeaccea3b44d5570f31c600f54d0159589
Franco <alu0101571669@ull.edu.es>
2024-10-01 19:19:27 +0100

    Tools

---
+++ otros/src/tools/tools.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+vector<vector<double>> ConstruirMatrizCoste(ifstream&);
+void MostrarMatriz(const vector<vector<double>>&);
