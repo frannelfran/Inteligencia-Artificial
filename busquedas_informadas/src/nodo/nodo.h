e7b6c9da0f8a4af67972ae4dee057b83391b07f4
alu0101571669 <alu0101571669@ull.edu.es>
2024-10-08 20:51:32 +0100

    Correción de funciones de la clase

--- busquedas_informadas/src/nodo/nodo.h
+++ busquedas_informadas/src/nodo/nodo.h
@@ -18,6 +18,7 @@ class Nodo {
   void SetHN(const int& h_n) { h_n_ = h_n; } // Establece la heurística del nodo
   void SetGN(const int& g_n) { g_n_ = g_n; } // Establece el costo del camino desde el nodo actual hasta el nodo objetivo
   void SetFN(const int& f_n) { f_n_ = f_n; } // Establece el costo total del nodo
+  void SetPadre(const Posicion& padre) { padre_ = padre; } // Establece el nodo padre
 
   // Getters
   int GetEstado() const { return estado_; } // Retorna el estado del nodo
@@ -25,6 +26,7 @@ class Nodo {
   int GetGN() const { return g_n_; } // Retorna el costo del camino desde el nodo actual hasta el nodo objetivo
   int GetHN() const { return h_n_; } // Retorna la heurística del nodo
   Posicion& GetPosicion() { return posicion_; } // Retorna la posición del nodo
+  Posicion& GetPadre() { return padre_; } // Retorna el nodo padre
 
   // Sobrecarga de operadores
   friend ostream& operator<<(ostream& os, const Nodo& nodo) {
@@ -38,8 +40,9 @@ class Nodo {
  private:
   int estado_; // Estado del nodo
   Posicion posicion_;
+  Posicion padre_;
   int costo_camino_; // Costo del camino desde el nodo raíz hasta el nodo actual
   int h_n_; // Heurística del nodo
-  int g_n_; // Costo del camino desde el nodo actual hasta el nodo objetivo
+  int g_n_ = 0; // Costo del camino desde el nodo actual hasta el nodo objetivo
   int f_n_; // Costo total del nodo
 };
