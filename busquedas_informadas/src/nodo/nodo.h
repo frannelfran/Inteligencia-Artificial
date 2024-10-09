a429f22eb65989fe064ed4b59efefd8b746ebfe6
alu0101571669 <alu0101571669@ull.edu.es>
2024-10-09 20:28:47 +0100

    Correción de la clase nodo para carácteres

--- busquedas_informadas/src/nodo/nodo.h
+++ busquedas_informadas/src/nodo/nodo.h
@@ -11,17 +11,17 @@ class Nodo {
  public:
   // Constructores
   Nodo() = default;
-  Nodo(const Posicion& posicion, const int& estado) : posicion_(posicion), estado_(estado) {}
+  Nodo(const Posicion& posicion, const int& estado) : posicion_(posicion), estado_(estado + '0') {}
 
   // Setters
-  void SetEstado(int estado) { estado_ = estado; } // Establece el estado del nodo
+  void SetEstado(char estado) { estado_ = estado; } // Establece el estado del nodo
   void SetHN(int h_n) { h_n_ = h_n; } // Establece la heurística del nodo
   void SetGN(int g_n) { g_n_ = g_n; } // Establece el costo del camino desde el nodo actual hasta el nodo objetivo
   void SetFN(int f_n) { f_n_ = f_n; } // Establece el costo total del nodo
   void SetPadre(Posicion padre) { padre_ = padre; } // Establece el nodo padre
 
   // Getters
-  int GetEstado() const { return estado_; } // Retorna el estado del nodo
+  char GetEstado() const { return estado_; } // Retorna el estado del nodo
   int GetFN() const { return f_n_; } // Retorna el costo total del nodo
   int GetGN() const { return g_n_; } // Retorna el costo del camino desde el nodo actual hasta el nodo objetivo
   int GetHN() const { return h_n_; } // Retorna la heurística del nodo
@@ -38,7 +38,7 @@ class Nodo {
   }
 
  private:
-  int estado_; // Estado del nodo
+  char estado_; // Estado del nodo
   Posicion posicion_;
   Posicion padre_;
   int costo_camino_; // Costo del camino desde el nodo raíz hasta el nodo actual
