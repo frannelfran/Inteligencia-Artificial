7ccf68c90e5296ba6e001482ce0a7c5f2293ec6a
alu0101571669 <alu0101571669@ull.edu.es>
2024-10-29 14:06:45 +0000

    Solucionespara la práctica 2

---
+++ prolog/practica_2/pregunta2.pl
@@ -0,0 +1,12 @@
+% Valores de X dentro de un rango de números
+
+generado_int(X, MIN, MAX) :- 
+  MIN =< MAX, 
+  X = MIN.
+
+generado_int(X, MIN, MAX) :-
+  MIN < MAX,
+  MIN1 is MIN + 1,
+  generado_int(X, MIN1, MAX).
+
+
