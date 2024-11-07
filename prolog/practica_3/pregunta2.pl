7a0daa99af683c5220e4d5d99bf75a617c9f4f06
alu0101571669 <alu0101571669@ull.edu.es>
2024-11-07 12:42:30 +0000

    Solución a la pregunta 2

---
+++ prolog/practica_3/pregunta2.pl
@@ -0,0 +1,7 @@
+% Caso base = listas vacias
+cuadrado([], []).
+
+% Caso recursivo
+cuadrado([H|T], [H2|T2]) :-
+    H2 is H*H, % Calcula el cuadrado de la cabeza
+    cuadrado(T, T2). % Calcula el cuadrado de la cola
