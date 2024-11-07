c7787b1653b25c2793fa444c5b6bd9bc74197956
alu0101571669 <alu0101571669@ull.edu.es>
2024-11-07 13:12:26 +0000

    Corregir solución 2

--- prolog/practica2/pregunta1.pl
+++
@@ -1,3 +0,0 @@
-% Saber si un número pertenece a un intervalo
-
-pertenece(X, Y, Z) :- X >= Y, X =< Z.
--- prolog/practica_3/pregunta2.pl
+++ prolog/practica_3/pregunta2.pl
@@ -3,5 +3,5 @@ cuadrado([], []).
 
 % Caso recursivo
 cuadrado([H|T], [H2|T2]) :-
-    H2 is H*H, % Calcula el cuadrado de la cabeza
-    cuadrado(T, T2). % Calcula el cuadrado de la cola
+  H2 is H*H, % Calcula el cuadrado de la cabeza
+  cuadrado(T, T2). % Calcula el cuadrado de la cola
