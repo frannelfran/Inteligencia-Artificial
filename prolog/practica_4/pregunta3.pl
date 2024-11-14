cee8733f7ed5c9486cc86b3f49f1272aa2cb31fe
alu0101571669 <alu0101571669@ull.edu.es>
2024-11-14 15:50:53 +0000

    Implementación de hanoi_dif

--- prolog/practica_4/pregunta3.pl
+++ prolog/practica_4/pregunta3.pl
@@ -8,10 +8,10 @@ hanoi(0, _, _, _, []).
 hanoi(N, Origen, Destino, Auxiliar, L) :-
   N > 0, % Si N es 0, no hay movimientos que hacer.
   N1 is N - 1, % Número de discos a mover en la llamada recursiva.
-  hanoi(N1, Origen, Auxiliar, Destino, L1), % Mueve N1 discos de A a B.
-  append(L1, [mover(N, Origen, Destino)], L2), % Agrega el movimiento actual.
-  hanoi(N1, Auxiliar, Destino, Origen, L3), % Mueve N1 discos de B a C.
-  append(L2, L3, L). % Concatena las listas de movimientos.
+  hanoi(N1, Origen, Auxiliar, Destino, L1),
+  append(L1, [mover(N, Origen, Destino)], L2),
+  hanoi(N1, Auxiliar, Destino, Origen, L3),
+  append(L2, L3, L).
 
 % hanoi_ACC
 hanoi_acc(N, L) :-
@@ -24,8 +24,24 @@ hanoi_acc(0, _, _, _, Acc, Acc).
 hanoi_acc(N, Origen, Destino, Auxiliar, Acc, L) :-
   N > 0, % Si N es 0, no hay movimientos que hacer.
   N1 is N - 1, % Número de discos a mover en la llamada recursiva.
-  hanoi_acc(N1, Origen, Auxiliar, Destino, Acc, L1), % Mueve N1 discos de A a B.
-  append(L1, [mover(N, Origen, Destino)], L2), % Agrega el movimiento actual.
-  hanoi_acc(N1, Auxiliar, Destino, Origen, L2, L). % Mueve N1 discos de B a C.
+  hanoi_acc(N1, Origen, Auxiliar, Destino, Acc, L1),
+  append(L1, [mover(N, Origen, Destino)], L2),
+  hanoi_acc(N1, Auxiliar, Destino, Origen, L2, L).
 
+% hanoi_dif
+
+hanoi_dif(N, L) :-
+  hanoi_dif(N, 1, 3, 2, L-Tail), % Llama a la relación hanoi_diff/6 con los valores iniciales.
+  Tail = [].
+
+% Caso base: si N es 0, no hay movimientos que hacer.
+hanoi_dif(0, _, _, _, L-L).
+
+% Caso recursivo: mueve N discos de la torre A a la torre C usando la torre B.
+hanoi_dif(N, Origen, Destino, Auxiliar, L-Tail) :-
+  N > 0, % Si N es 0, no hay movimientos que hacer.
+  N1 is N - 1, % Número de discos a mover en la llamada recursiva.
+  hanoi_dif(N1, Origen, Auxiliar, Destino, L-L1),
+  L1 = [mover(Origen, Destino) | L2],
+  hanoi_dif(N1, Auxiliar, Destino, Origen, L2-Tail).
 
