0f7d90170e39917f42c47edec25f6366f9f1c65d
alu0101571669 <alu0101571669@ull.edu.es>
2024-11-21 18:15:09 +0000

    Correción de la pregunta 4

--- prolog/practica_5/pregunta4.pl
+++ prolog/practica_5/pregunta4.pl
@@ -1,17 +1,17 @@
 % Caso base: 2 es primo.
 primo(2) :- !.
+primo(3) :- !.
 
 % Caso general: un número es primo si es mayor que 2, no es divisible por ningún número
 % entre 2 y la raíz cuadrada de N.
 primo(N) :-
-  N > 2,
-  N mod 2 =\= 0,           % Descarta números pares mayores que 2.
-  \+ tiene_divisor(N, 3).  % Comprueba si hay divisores comenzando desde 3.
+  N > 1,
+  not(tiene_divisor(N, 2)).
 
 % Predicado auxiliar para verificar si N tiene divisores.
 tiene_divisor(N, Div) :-
   Div * Div =< N,          % Solo verifica hasta la raíz cuadrada de N.
   (N mod Div =:= 0;        % Falla si encuentra un divisor exacto.
-    NextDiv is Div + 2,     % Incrementa el divisor en 2 (solo impares).
+    NextDiv is Div + 1,     % Incrementa el divisor en 2 (solo impares).
     tiene_divisor(N, NextDiv)).
 
