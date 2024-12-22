a055a92592572d00395af5f1c095548ccc65cba8
alu0101571669 <alu0101571669@ull.edu.es>
2024-12-22 18:40:01 +0000

    Programa principal

---
+++ busqueda_no_informada_2.0/main.cc
@@ -0,0 +1,33 @@
+#include "nodo/nodo.h"
+#include "grafo/grafo.h"
+
+vector<vector<double>> MatrizCoste(ifstream& file) {
+  double coste;
+  int num_nodos;
+  file >> num_nodos;
+  vector<vector<double>> matriz(num_nodos, vector<double>(num_nodos, 0));
+  for (int i = 0; i < num_nodos; i++) {
+    for (int j = i + 1; j < num_nodos; j++) {
+      file >> coste;
+      if (coste != -1) {
+        matriz[i][j] = coste;
+        matriz[j][i] = coste;
+      }
+    }
+  }
+  return matriz;
+}
+
+int main(int argc, char* argv[]) {
+  ifstream file(argv[1]);
+  vector<vector<double>> matriz = MatrizCoste(file);
+
+  // Crear grafo
+  Grafo grafo(matriz);
+  cout << grafo;
+  Nodo inicial(15);
+  Nodo final(1);
+  grafo.RecorridoProfundidad(inicial, final);
+  
+  return 0;
+}
