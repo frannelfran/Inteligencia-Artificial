d61a35c3757702b8aa550c378aafac1ecfb4cbc9
alu0101571669 <alu0101571669@ull.edu.es>
2024-12-23 15:05:11 +0000

    Cambiar nodo inicial y final

--- busqueda_no_informada_2.0/main.cc
+++ busqueda_no_informada_2.0/main.cc
@@ -5,14 +5,13 @@
 int main(int argc, char* argv[]) {
   Tools options = parse_args(argc, argv);
 
-  // Fichero de entrada, salida y nodos inicial y final
+  // Fichero de entrada, salida
   ifstream input(options.fichero_entrada);
   ofstream output(options.fichero_salida);
-  Nodo inicial(options.NodoInicial);
-  Nodo final(options.NodoFinal);
 
   // Matriz de costes
   vector<vector<double>> matriz = MatrizCoste(input);
+
   // Creación del grafo
   Grafo grafo(matriz);
 
@@ -20,15 +19,29 @@ int main(int argc, char* argv[]) {
 
   while (true) {
     cout << "1. Recorrido en profundidad" << endl;
-    cout << "2. Salir" << endl;
+    cout << "2. Recorrido en amplitud" << endl;
+    cout << "3. Cambiar nodos inicial y final" << endl;
+    cout << "4. Salir" << endl;
     cout << "Introducir opción: ";
     cin >> opcion;
 
     switch (opcion) {
-      case 1:
+      case 1: {
+        Nodo inicial(options.NodoInicial);
+        Nodo final(options.NodoFinal);
         grafo.RecorridoProfundidad(inicial, final, output);
+        cout << "Comprobar fichero de salida" << endl;
         break;
+      }
       case 2:
+        return 0;
+      case 3:
+        cout << "Introducir nodo inicial: ";
+        cin >> options.NodoInicial;
+        cout << "Introducir nodo final: ";
+        cin >> options.NodoFinal;
+        break;
+      case 4:
         return 0;
       default:
         cout << "Opción no válida" << endl;
